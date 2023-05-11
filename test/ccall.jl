e2c7e6eda3856ed66cf95cdf6547bad0a89bfa0d
Valentin Churavy <v.churavy@gmail.com>
2023-05-11 12:02:51 -0400

    Allow for :foreigncall to transition to GC safe automatically
    Co-authored-by: Gabriel Baraldi <baraldigabriel@gmail.com>

--- test/ccall.jl
+++ test/ccall.jl
@@ -1966,3 +1966,14 @@ let llvm = sprint(code_llvm, world_counter, ())
     # the world age should be -1 in generated functions (or other pure contexts)
     @test (generated_world_counter() == reinterpret(UInt, -1))
 end
+
+function gc_safe_ccall()
+    @ccall gc_safe=true jl_get_cpu_features()::String
+end
+
+let llvm = sprint(code_llvm, gc_safe_ccall, ())
+    # check that the call works
+    @test gc_safe_ccall() isa String
+    # check for the gc_safe store
+    @test occursin("store atomic i8 2", llvm)
+end
