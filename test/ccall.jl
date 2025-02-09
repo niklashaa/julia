ce14078ee158183e9d59be1767ed3931d816bbd7
Valentin Churavy <vchuravy@users.noreply.github.com>
2025-02-09 15:51:16 +0100

    Fixup test

--- test/ccall.jl
+++ test/ccall.jl
@@ -1974,7 +1974,7 @@ end
 
 let llvm = sprint(code_llvm, gc_safe_ccall, ())
     # check that the call works
-    @test gc_safe_ccall() isa String
+    @test gc_safe_ccall() isa Nothing
     # check for the gc_safe store
     @test occursin("store atomic i8 2", llvm)
 end
